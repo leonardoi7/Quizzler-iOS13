16a2677c88e302c655365ff726af55b7ca2d27d6
leonardoi7 <6802237+leonardoi7@users.noreply.github.com>
2020-05-20 22:37:50 -0700

    Added mock questions
    Working Quiz game with three questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    let quiz = [
+        ["A slug's blood is green.","True"],
+        ["Approximately one quarter of human bones are in the feet.","True"],
+        ["The total surface area of two human lungs is approximately 70 square metres.", "False"],
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle // True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Good")
+        }else{
+            print("Wrong")
+        }
+        
+        questionNumber += 1
+        if questionNumber > 2{
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+            
+    }
+    
+    
 }
 
